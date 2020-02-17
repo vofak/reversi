df821ce6a09b8401398df9f9a3d9f10fd2eb04d6
vojtech.foret <vofakcz@gmail.com>
2020-02-17 13:35:24 +0100

    Updates

---
+++ precache-manifest.0bb8686514d0704cf61712778558818e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e615f8f066cae7efc26ba793496ea95",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e63eea877040c8b62000",
+    "url": "./static/css/main.2f3c7407.chunk.css"
+  },
+  {
+    "revision": "dbd35ce864c739db754e",
+    "url": "./static/js/2.f2e1912f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.f2e1912f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e63eea877040c8b62000",
+    "url": "./static/js/main.74c0be92.chunk.js"
+  },
+  {
+    "revision": "4f464d669b381e1b90c0",
+    "url": "./static/js/runtime-main.41ad8700.js"
+  }
+]);
